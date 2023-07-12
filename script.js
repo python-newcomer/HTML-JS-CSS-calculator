3d165fe9d5d237c9ac398bd1a09cec01b7807730
python-newcomer <128653962+python-newcomer@users.noreply.github.com>
2023-07-12 15:07:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function add(){
+    let a=parseInt(document.getElementById("n1").value);
+    let b=parseInt(document.getElementById("n2").value);
+    let c=a+b;
+    document.getElementById("button").innerHTML="Addition : "+c;
+}
+function sub(){
+    let a=parseInt(document.getElementById("n1").value);
+    let b=parseInt(document.getElementById("n2").value);
+    let c=a-b;
+    document.getElementById("button").innerHTML="Subtraction : "+c;
+}
+function mul(){
+    let a=parseInt(document.getElementById("n1").value);
+    let b=parseInt(document.getElementById("n2").value);
+    let c=a*b;
+    document.getElementById("button").innerHTML="Multiplication : "+c;
+}
+function div(){
+    let a=parseInt(document.getElementById("n1").value);
+    let b=parseInt(document.getElementById("n2").value);
+    let c=a/b;
+    document.getElementById("button").innerHTML="Division : "+c;
+}
+function mod(){
+    let a=parseInt(document.getElementById("n1").value);
+    let b=parseInt(document.getElementById("n2").value);
+    let c=a%b;
+    document.getElementById("button").innerHTML="Modulus : "+c;
+}
+function floor(){
+    let a=parseInt(document.getElementById("n1").value);
+    let b=parseInt(document.getElementById("n2").value);
+    let c=a//b;
+    document.getElementById("button").innerHTML="Floor division : "+c;
+}
+
+
+
+
